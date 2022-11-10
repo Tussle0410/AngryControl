6b3b49bcfc5c3b4d79002adc0f782dba1d33976a
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-11-11 07:35:06 +0900

    Serializable Interface Add

--- app/src/main/kotlin/com/tussle/angrycontrol/model/DateAndDiary.kt
+++ app/src/main/kotlin/com/tussle/angrycontrol/model/DateAndDiary.kt
@@ -1,10 +1,11 @@
 package com.tussle.angrycontrol.model
 
 import androidx.room.Embedded
+import java.io.Serializable
 
 data class DateAndDiary(
     @Embedded
     val angryDate: AngryDate,
     @Embedded
     val angryDiary: AngryDiary
-)
+) : Serializable
