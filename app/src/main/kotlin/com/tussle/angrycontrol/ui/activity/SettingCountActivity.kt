27237a4eefb61ba926d4c773948286d4e52ffcf7
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-11-27 06:14:41 +0900

    startCount & countEffect Setting Update

--- app/src/main/kotlin/com/tussle/angrycontrol/ui/activity/SettingCountActivity.kt
+++ app/src/main/kotlin/com/tussle/angrycontrol/ui/activity/SettingCountActivity.kt
@@ -1,12 +1,15 @@
 package com.tussle.angrycontrol.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.tussle.angrycontrol.R
 import com.tussle.angrycontrol.databinding.SettingCountLayoutBinding
+import com.tussle.angrycontrol.sharedPreference.GlobalApplication
 import com.tussle.angrycontrol.viewmodel.SettingCountViewModel
+import java.lang.IllegalArgumentException
 
 class SettingCountActivity : AppCompatActivity() {
     private val viewModel : SettingCountViewModel by lazy {
@@ -22,10 +25,52 @@ class SettingCountActivity : AppCompatActivity() {
     }
     private fun init(){
         setButton()
+        checkRadio()
+        setRadioButton()
     }
     private fun setButton(){
         binding.settingCountBackButton.setOnClickListener {
             finish()
+        }
+    }
+    private fun checkRadio(){
+        when(viewModel.countStart) {
+            "10" -> binding.settingCountStartTime10.isChecked = true
+            "20" -> binding.settingCountStartTime20.isChecked = true
+            "30" -> binding.settingCountStartTime30.isChecked = true
+            "60" -> binding.settingCountStartTime60.isChecked = true
+            else -> throw IllegalArgumentException("Not Found CountStartTime")
+        }
+        when(viewModel.countEffect) {
+            "Vibration" -> binding.settingCountCountEffectVibration.isChecked = true
+            "Sound" -> binding.settingCountCountEffectSound.isChecked = true
+            "Sound+Vibration" -> binding.settingCountCountEffectVibrationAndSound.isChecked = true
+            "NotEffect" -> binding.settingCountCountEffectNotEffect.isChecked = true
+            else -> throw IllegalArgumentException("Not Found CountEffect")
+        }
+    }
+    private fun setRadioButton(){
+        binding.settingCountStartTimeRadio.setOnCheckedChangeListener { _, i ->
+            with(GlobalApplication.pref){
+                when(i) {
+                    R.id.setting_count_startTime_10 -> this.settingSetString("countStart", "10")
+                    R.id.setting_count_startTime_20 -> this.settingSetString("countStart", "20")
+                    R.id.setting_count_startTime_30 -> this.settingSetString("countStart", "30")
+                    R.id.setting_count_startTime_60 -> this.settingSetString("countStart", "60")
+                    else -> throw IllegalArgumentException("Not Found Count RadioButton ID")
+                }
+            }
+        }
+        binding.settingCountEffectRadio.setOnCheckedChangeListener { b , i ->
+            with(GlobalApplication.pref){
+                when(i) {
+                    R.id.setting_count_countEffect_vibration -> this.settingSetString("countEffect", "Vibration")
+                    R.id.setting_count_countEffect_sound -> this.settingSetString("countEffect", "Sound")
+                    R.id.setting_count_countEffect_vibrationAndSound -> this.settingSetString("countEffect", "Sound+Vibration")
+                    R.id.setting_count_countEffect_notEffect -> this.settingSetString("countEffect", "NotEffect")
+                    else -> throw IllegalArgumentException("Not Found Effect RadioButton ID")
+                }
+            }
         }
     }
 }
--- app/src/main/kotlin/com/tussle/angrycontrol/viewmodel/SettingCountViewModel.kt
+++ app/src/main/kotlin/com/tussle/angrycontrol/viewmodel/SettingCountViewModel.kt
@@ -1,6 +1,9 @@
 package com.tussle.angrycontrol.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.tussle.angrycontrol.sharedPreference.GlobalApplication
 
 class SettingCountViewModel : ViewModel() {
+    val countStart = GlobalApplication.pref.settingGetString("countStart", "10")
+    val countEffect = GlobalApplication.pref.settingGetString("countEffect", "Vibration")
 }
