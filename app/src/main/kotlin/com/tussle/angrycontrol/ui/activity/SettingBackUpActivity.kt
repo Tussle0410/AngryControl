6f04ff82f934daf513bd944becf066f59d867776
Tussle0410 <69793388+Tussle0410@users.noreply.github.com>
2022-11-28 07:15:39 +0900

    RoomDB BackUp & Restore Function Update

--- app/src/main/kotlin/com/tussle/angrycontrol/ui/activity/SettingBackUpActivity.kt
+++ app/src/main/kotlin/com/tussle/angrycontrol/ui/activity/SettingBackUpActivity.kt
@@ -6,13 +6,17 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.tussle.angrycontrol.R
 import com.tussle.angrycontrol.databinding.SettingBackupLayoutBinding
+import com.tussle.angrycontrol.sharedPreference.GlobalApplication
 import com.tussle.angrycontrol.viewmodel.SettingBackUpViewModel
+import de.raphaelebner.roomdatabasebackup.core.RoomBackup
 
 class SettingBackUpActivity : AppCompatActivity() {
     private val viewModel : SettingBackUpViewModel by lazy {
         ViewModelProvider(this).get(SettingBackUpViewModel::class.java)
     }
     private lateinit var binding : SettingBackupLayoutBinding
+    private val backUp = RoomBackup(this)
+    private val restore = RoomBackup(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.setting_backup_layout)
@@ -21,11 +25,41 @@ class SettingBackUpActivity : AppCompatActivity() {
         init()
     }
     private fun init(){
+        backUpInit()
+        restoreInit()
         setButton()
     }
     private fun setButton(){
         binding.settingBackupBackButton.setOnClickListener {
             finish()
         }
+        binding.settingBackupBackup.setOnClickListener {
+            backUp.backup()
+        }
+        binding.settingBackupRestore.setOnClickListener {
+            restore.restore()
+        }
+    }
+    private fun backUpInit(){
+        backUp
+            .database(GlobalApplication.DBInstance)
+            .enableLogDebug(true)
+            .backupIsEncrypted(true)
+            .backupLocation(RoomBackup.BACKUP_FILE_LOCATION_CUSTOM_DIALOG)
+            .maxFileCount(5)
+            .apply {
+                onCompleteListener { success, _, _ ->
+                    if(success)
+                        viewModel.setBackUpDate()
+                }
+            }
+    }
+    private fun restoreInit(){
+        restore
+            .database(GlobalApplication.DBInstance)
+            .enableLogDebug(true)
+            .backupIsEncrypted(true)
+            .backupLocation(RoomBackup.BACKUP_FILE_LOCATION_CUSTOM_DIALOG)
+            .maxFileCount(5)
     }
 }
--- app/src/main/kotlin/com/tussle/angrycontrol/viewmodel/SettingBackUpViewModel.kt
+++ app/src/main/kotlin/com/tussle/angrycontrol/viewmodel/SettingBackUpViewModel.kt
@@ -1,6 +1,17 @@
 package com.tussle.angrycontrol.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.tussle.angrycontrol.sharedPreference.GlobalApplication
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 class SettingBackUpViewModel : ViewModel() {
+    var backUpDate = GlobalApplication.pref.settingGetString("backUpDate","")
+    private val formatter = SimpleDateFormat("yyyy-MM-dd")
+    fun setBackUpDate(){
+        val date = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+        backUpDate = formatter.format(date).toString()
+        GlobalApplication.pref.settingSetString("backUpDate", backUpDate)
+    }
 }
